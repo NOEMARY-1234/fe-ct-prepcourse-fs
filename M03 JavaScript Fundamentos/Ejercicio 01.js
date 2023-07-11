8bb689e297345b47afa92fb0c490e78ec61c6397
maria <gmarianoemi78@gmail.com>
2023-07-10 21:44:18 -0300

    resultados

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -6,22 +6,27 @@
 
 // Crea una variable de tipo string.
 const nuevoString = null;
-
+var bootcamp="henry";
+console.log("henry");
 // Crea una variable de tipo number.
 const nuevoNumero = null;
-
+var numeroentero=16;
+console.log(16);
 // Crea una variable de tipo boolean.
 const nuevoBoolean = null;
-
+var estoyentendiendo=true;
+console.log(estoyentendiendo)
 // Resuelve el siguiente problema matemático.
-const nuevaResta = 10 - null === 3;
-
+const nuevaResta = 10 - null === 3
+10-7
+console.log(10-7);
 // Resuelve el siguiente problema matemático.
 const nuevaMultiplicacion = 10 * null === 40;
-
+10*4
+console.log(10*4);
 // Resuelve el siguiente problema matemático.
 const nuevoModulo = 21 % 5 === null;
-
+console.log(21%5);
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
    nuevoString,
